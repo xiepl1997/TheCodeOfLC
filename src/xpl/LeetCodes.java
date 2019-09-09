7b8b4c5246ac823241fa46e729251124da4d11e0
xiepl1997 <xiepl1997@gmail.com>
2019-09-09 14:31:53 +0800

    add 54 螺旋矩阵

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -600,6 +600,43 @@ public class LeetCodes {
     }
 
 	/**
+	 * 54 螺旋矩阵
+	 * @param matrix
+	 * @return
+	 */
+	public List<Integer> spiralOrder(int[][] matrix) {
+		List<Integer> result = new ArrayList<>();
+		if(matrix.length == 0)
+			return result;
+		int m = matrix[0].length;
+		int n = matrix.length;
+		int up = 0, down = n-1, left = 0, right = m-1;
+		while(true){
+			for(int i = left; i <= right; i++){
+				result.add(matrix[up][i]);
+			}
+			if(++up > down)
+				break;
+			for(int i = up; i <= down; i++){
+				result.add(matrix[i][right]);
+			}
+			if(--right < left)
+				break;
+			for(int i = right; i >= left; i--){
+				result.add(matrix[down][i]);
+			}
+			if(--down < up)
+				break;
+			for(int i = down; i >= up; i--){
+				result.add(matrix[i][left]);
+			}
+			if(++left > right)
+				break;
+		}
+		return result;
+	}
+
+	/**
 	 * 62 不同路径，m*n矩阵中左上角到右下角的路径数量
 	 * @param m
 	 * @param n
