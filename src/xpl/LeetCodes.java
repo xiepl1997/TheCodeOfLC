6d7a687e8659b78e2ad856654eb23163241acfcf
xiepl1997 <xiepl1997@gmail.com>
2019-09-23 21:23:08 +0800

    add 416 分割等和子集

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -1508,6 +1508,43 @@ public class LeetCodes {
 	}
 
 	/**
+	 * 416 分割等和子集，给定一个只包含正整数的非空数组，问是否可以将这个数组分割成两个子集，使得两个子集的元素和相等
+	 * 分析：求得数组中是否存在加起来为sum/2的数
+	 * @param nums
+	 * @return
+	 */
+	public boolean canPartition(int[] nums) {
+		int sum = 0;
+		for(int num : nums){
+			sum += num;
+		}
+		if(sum % 2 != 0)
+			return false;
+		int[][] dp = new int[nums.length+1][sum/2+1];
+		for(int i = 0; i < dp.length; i++){
+			dp[i][0] = 0;
+		}
+		for(int i = 0; i < dp[0].length; i++){
+			dp[0][i] = 0;
+		}
+		for(int i = 1; i < nums.length; i++){
+			for(int j = 1; j < dp[0].length; j++){
+				if(nums[i] > j){
+					dp[i][j] = dp[i-1][j];
+				}
+				else{
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-nums[i]]+nums[i]);
+				}
+				if(dp[i][j] == sum/2)
+					return true;
+			}
+		}
+		return false;
+
+
+	}
+
+	/**
 	 * 887 鸡蛋掉落
 	 * @param K
 	 * @param N
