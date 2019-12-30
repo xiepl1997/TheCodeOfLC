9930b6199d357057caafded6e397d6653036a37d
xiepl1997 <xiepl1997@gmail.com>
2019-12-30 15:49:21 +0800

    add 162 寻找峰值（二分）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -2311,6 +2311,23 @@ public class LeetCodes {
 		return list.get(list.size()-1);
 	}
 
+	/**
+	 * 162 寻找峰值(二分）
+	 * @param nums
+	 * @return
+	 */
+	public int findPeakElement(int[] nums) {
+		int left = 0, right = nums.length-1, mid = 0;
+		while(left < right){
+			mid = left+(right-left)/2;
+			if(nums[mid] > nums[mid+1])
+				right = mid;
+			else
+				left = mid+1;
+		}
+		return left;
+	}
+
     /**
      * 169 求众数
      * @param nums
