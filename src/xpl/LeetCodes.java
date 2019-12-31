f5eb4f3f4a87cc7a0b8e167b8ad1416e43241f83
xiepl1997 <xiepl1997@gmail.com>
2019-12-31 23:51:12 +0800

    add 191 位1 的个数（32位int）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -2401,6 +2401,22 @@ public class LeetCodes {
 	 */
 
 	/**
+	 * 191 位1的个数
+	 * @param n
+	 * @return
+	 */
+	public int hammingWeight(int n) {
+		int res = 0;
+		int mask = 1;
+		for(int i = 0; i < 32; i++){
+			if((n & mask) != 0)
+				res++;
+			mask<<=1;
+		}
+		return res;
+	}
+
+	/**
 	 * 204 计算质数，厄拉多赛筛法求素数
 	 * @param n
 	 * @return
