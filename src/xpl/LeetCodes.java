206a95d3fbcc9ca9a0b67fd41df8881051c6a88d
xiepl1997 <xiepl1997@gmail.com>
2020-02-10 21:08:01 +0800

    add 91 解码方法（类似爬楼梯）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -1943,6 +1943,26 @@ public class LeetCodes {
 	}
 
 	/**
+	 * 91 解码方法
+	 * @param s
+	 * @return
+	 */
+	public int numDecodings(String s) {
+		if(s == null || s.charAt(0)=='0'){
+			return 0;
+		}
+		int[] dp = new int[s.length()+1];
+		dp[0] = 1;
+		for(int i = 1; i < dp.length; i++){
+			dp[i] = (s.charAt(i-1)=='0'?0:dp[i-1]);
+			if(i >= 2 && (s.charAt(i-2) == '1' || (s.charAt(i-2) == '2' && s.charAt(i-1) <= '6'))){
+				dp[i] += dp[i-2];
+			}
+		}
+		return dp[dp.length-1];
+	}
+
+	/**
 	 * 94 二叉树的中序遍历（递归）
 	 * @param root
 	 * @return
