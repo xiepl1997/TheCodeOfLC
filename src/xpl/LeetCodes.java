050cc8cd4ab01cb6811b95150ef97c41c84acb7c
xiepl1997 <xiepl1997@gmail.com>
2020-02-10 21:42:36 +0800

    add 198 打家劫舍（简单动态规划）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -2846,6 +2846,27 @@ public class LeetCodes {
 	}
 
 	/**
+	 * 198 打家劫舍
+	 * 动态规划
+	 * @param nums
+	 * @return
+	 */
+	public int rob(int[] nums) {
+		if(nums == null || nums.length == 0)
+			return 0;
+		int[] dp = new int[nums.length];
+		dp[0] = nums[0];
+
+		if(nums.length > 1){
+			dp[1] = Math.max(nums[0], nums[1]);
+		}
+		for(int i = 2; i < nums.length; i++){
+			dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
+		}
+		return dp[dp.length-1];
+	}
+
+	/**
 	 * 204 计算质数，厄拉多赛筛法求素数
 	 * @param n
 	 * @return
