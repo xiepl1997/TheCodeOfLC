864dadb238a702f0a34ef3aff3d9d1b8d89feab6
xiepl1997 <xiepl1997@gmail.com>
2020-02-11 14:12:30 +0800

    add 120 三角形最小路径和（动态规划）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -2419,6 +2419,61 @@ public class LeetCodes {
 	}
 
 	/**
+	 * 120 三角形最小路径和
+	 * dp动态规划（自顶向下）
+	 * @param triangle
+	 * @return
+	 */
+	public int minimumTotal(List<List<Integer>> triangle) {
+		if(triangle == null || triangle.size() == 0)
+			return 0;
+		int[][] dp = new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+		dp[0][0] = triangle.get(0).get(0);
+		if(triangle.size() == 1)
+			return dp[0][0];
+		dp[1][0] = dp[0][0] + triangle.get(1).get(0);
+		dp[1][1] = dp[0][0] + triangle.get(1).get(1);
+		for(int i = 2; i < triangle.size(); i++){
+			for(int j = 0; j < triangle.get(i).size(); j++){
+				//最左侧
+				if(j == 0){
+					dp[i][j] = dp[i-1][j] + triangle.get(i).get(j);
+				}
+				//最右侧
+				else if(j == triangle.get(i).size()-1){
+					dp[i][j] = dp[i-1][j-1] + triangle.get(i).get(j);
+				}
+				else{
+					dp[i][j] = Math.min(dp[i-1][j-1]+triangle.get(i).get(j), dp[i-1][j]+triangle.get(i).get(j));
+				}
+			}
+		}
+		int min = Integer.MAX_VALUE;
+		for(int i = 0; i < dp[dp.length-1].length; i++){
+			min = Math.min(min, dp[dp.length-1][i]);
+		}
+		return min;
+	}
+
+	/**
+	 * 120 三角形最小路径和
+	 * dp动态规划（自底向上）
+	 * @param triangle
+	 * @return
+	 */
+	public int minimumTotal_1(List<List<Integer>> triangle) {
+		if(triangle == null || triangle.size() == 0)
+			return 0;
+		for(int i = triangle.size()-2; i >= 0; i--){
+			for(int j = 0; j < triangle.get(i).size(); j++){
+				int temp = triangle.get(i).get(j) + Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1));
+				triangle.get(i).set(j, temp);
+			}
+		}
+		return triangle.get(0).get(0);
+	}
+
+	/**
 	 * 121 买卖股票的最佳时机
 	 * @param prices
 	 * @return
