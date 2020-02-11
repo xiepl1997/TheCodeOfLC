76453d6e6f3d273a4494c2d4734bfef3525ae038
xiepl1997 <xiepl1997@gmail.com>
2020-02-11 22:52:09 +0800

    add 面试题04 二维数组中的查找

--- src/xpl/PointToOffer.java
+++ src/xpl/PointToOffer.java
@@ -24,4 +24,26 @@ public class PointToOffer {
         }
         return -1;
     }
+
+    /**
+     * 二维数组中的查找
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0 )
+            return false;
+        int i = 0; //右上角的点的x坐标
+        int j = matrix[0].length-1; //右上角的点的y坐标
+        while(i <= matrix.length && j >= 0){
+            if(target == matrix[i][j])
+                return true;
+            else if(target < matrix[i][j])
+                j--;
+            else
+                i++;
+        }
+        return false;
+    }
 }
