a9acf2781eea5565c2a417238b94f3bf71582c30
xiepl1997 <xiepl1997@gmail.com>
2020-03-02 19:10:14 +0800

    add 面试题47 礼物的最大价值

--- src/xpl/PointToOffer.java
+++ src/xpl/PointToOffer.java
@@ -803,6 +803,34 @@ public class PointToOffer {
     }
 
     /**
+     * 面试题47 礼物的最大价值
+     * @param grid
+     * @return
+     */
+    public int maxValue(int[][] grid) {
+        if(grid == null || grid.length == 0 )
+            return 0;
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        int maxvalue = dp[0][0];
+        for(int i = 1; i < grid[0].length; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+            maxvalue = Math.max(maxvalue, dp[0][i]);
+        }
+        for(int i = 1; i < grid.length; i++){
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+            maxvalue = Math.max(maxvalue, dp[i][0]);
+        }
+        for(int i = 1; i < grid.length; i++){
+            for(int j = 1; j < grid[0].length; j++){
+                dp[i][j] = Math.max(grid[i][j]+dp[i-1][j], grid[i][j]+dp[i][j-1]);
+                maxvalue = Math.max(maxvalue, dp[i][j]);
+            }
+        }
+        return maxvalue;
+    }
+
+    /**
      * 面试题50 第一个只出现一次的字符
      * 例如 abaccdeff，返回 b
      * @param s
