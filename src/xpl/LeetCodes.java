b9a244150719a7512a5a8ed1bb8ee5a70da3095b
xiepl1997 <xiepl1997@gmail.com>
2020-03-12 20:01:10 +0800

    添加 1071 字符串的最大公因子（辗转相除法）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -4135,6 +4135,21 @@ public class LeetCodes {
 		return count == 3;
 	}
 
+	/**
+	 * 1071 字符串的最大公因子（辗转相除法）
+	 * @param str1
+	 * @param str2
+	 * @return
+	 */
+	public String gcdOfStrings(String str1, String str2) {
+		if(!(str1+str2).equals(str2+str1))
+			return "";
+		return str1.substring(0, gcd(str1.length(), str2.length()));
+	}
+	public int gcd(int a, int b){
+		return b == 0 ? a : gcd(b, a % b);
+	}
+
     /**
      * 1103 分糖果Ⅱ
      * @param candies
