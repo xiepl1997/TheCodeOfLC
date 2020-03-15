ef40fd2140f12a23416b0c0ad88bba8e39966987
xiepl1997 <xiepl1997@gmail.com>
2020-03-15 14:20:49 +0800

    add 695 岛屿的最大面积（深度优先遍历）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -4018,6 +4018,46 @@ public class LeetCodes {
 //	}
 
     /**
+     * 695 岛屿的最大面积（深度优先遍历）
+     * @param grid
+     * @return
+     */
+    int flag_695[][];
+    int max_695 = 0;
+    int temp_695 = 0;
+    int[] x_695 = new int[]{1,0,-1,0};
+    int[] y_695 = new int[]{0,1,0,-1};
+    public int maxAreaOfIsland(int[][] grid) {
+        if(grid == null || grid.length == 0)
+            return 0;
+        flag_695 = new int[grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                temp_695 = 0;
+                dfs_695(i, j, grid);
+                max_695 = max_695 < temp_695 ? temp_695 : max_695;
+            }
+        }
+        return max_695;
+    }
+    public void dfs_695(int i, int j, int[][] grid){
+        if(flag_695[i][j] == 1)
+            return;
+        if(grid[i][j] == 0){
+            flag_695[i][j] = 1;
+            return;
+        }
+        if(grid[i][j] == 1){
+            flag_695[i][j] = 1;
+            temp_695 ++;
+        }
+        for(int k = 0; k < 4; k++){
+            if(i+x_695[k] >= 0 && i+x_695[k] < grid.length && j+y_695[k] >= 0 && j+y_695[k] < grid[0].length)
+                dfs_695(i+x_695[k], j+y_695[k], grid);
+        }
+    }
+
+    /**
      * 946 验证栈序列
      * @param pushed
      * @param popped
