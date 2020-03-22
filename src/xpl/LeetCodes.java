c5daeacdf3706d6c91524df8b761fb2ce95f4f15
xiepl1997 <xiepl1997@gmail.com>
2020-03-22 10:31:01 +0800

    add 945 使数组唯一的最小增量

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -4105,6 +4105,24 @@ public class LeetCodes {
 		return true;
 	}
 
+	/**
+	 * 945 使数组唯一的最小增量
+	 * @param A
+	 * @return
+	 */
+	public int minIncrementForUnique(int[] A) {
+		Arrays.sort(A);
+		int res = 0;
+		for(int i = 1; i < A.length; i++){
+			if(A[i] <= A[i-1]){
+				int temp = A[i];
+				A[i] = A[i-1]+1;
+				res += A[i]-temp;
+			}
+		}
+		return res;
+	}
+
     /**
      * 946 验证栈序列
      * @param pushed
