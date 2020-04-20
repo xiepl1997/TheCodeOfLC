c7383c993af1a947ff156610d57523393a91e019
xiepl1997 <xiepl1997@gmail.com>
2020-04-20 12:45:35 +0800

    add 200 岛屿数量dfs

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -3176,6 +3176,34 @@ public class LeetCodes {
 	}
 
 	/**
+	 * 200 岛屿数量， dfs
+	 * @param grid
+	 * @return
+	 */
+	public int numIslands(char[][] grid) {
+		int res = 0;
+		if(grid == null || grid.length == 0)
+			return res;
+		int m = grid.length;
+		int n = grid[0].length;
+		for(int i = 0; i < m; i++){
+			for(int j = 0; j < n; j++){
+				if(grid[i][j] == '1'){
+					res++;
+					dfs_200(grid, i, j);
+				}
+			}
+		}
+		return res;
+	}
+	public void dfs_200(char[][] grid, int i, int j){
+		grid[i][j] = '0';
+		if(i + 1 < grid.length && grid[i+1][j] == '1') dfs_200(grid, i+1, j);
+		if(i - 1 >= 0 && grid[i-1][j] == '1') dfs_200(grid, i-1, j);
+		if(j + 1 < grid[0].length && grid[i][j+1] == '1') dfs_200(grid, i, j + 1);
+		if(j - 1 >= 0 && grid[i][j-1] == '1') dfs_200(grid, i, j - 1);
+	}
+	/**
 	 * 204 计算质数，厄拉多赛筛法求素数
 	 * @param n
 	 * @return
