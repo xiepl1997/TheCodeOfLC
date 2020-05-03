b153723dc21614ade0c88f49e8e220d4361eab9e
xiepl1997 <xiepl1997@gmail.com>
2020-05-03 22:59:13 +0800

    add 108 将有序数组转化为二叉搜索树（高度平衡二叉树）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -2382,6 +2382,23 @@ public class LeetCodes {
 	}
 
 	/**
+	 * 108 将有序数组转化为二叉搜索树（高度平衡二叉树）
+	 * @param nums
+	 * @return
+	 */
+	public TreeNode sortedArrayToBST(int[] nums) {
+		return helper(nums, 0, nums.length - 1);
+	}
+	public TreeNode helper(int[] nums, int left, int right){
+		if(left > right) return null;
+		int p = (left + right) / 2;
+		TreeNode root = new TreeNode(nums[p]);
+		root.left = helper(nums, left, p-1);
+		root.right = helper(nums, p +1 ,right);
+		return root;
+	}
+
+	/**
 	 * 110 平衡二叉树
 	 * 给定一个二叉树，判断是否是平衡二叉树
 	 * @param root
