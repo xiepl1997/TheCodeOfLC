7ee0509f16e050d4ac54ebc460a60507a4f4de92
xiepl1997 <xiepl1997@gmail.com>
2020-05-06 14:19:56 +0800

    add 983 最低票价（动态规划）

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -4381,6 +4381,39 @@ public class LeetCodes {
         return j == len;
     }
 
+	/**
+	 * 983 最低票价（动态规划）
+	 * @param days
+	 * @param costs
+	 * @return
+	 */
+	public int mincostTickets(int[] days, int[] costs) {
+		int[] dp = new int[days.length];
+		dp[0] = Math.min(costs[0], Math.min(costs[1], costs[2]));
+
+		for(int i = 1; i < days.length; i++){
+			int c1 = dp[i-1] + costs[0];
+			int c2 = Integer.MAX_VALUE;
+			int c3 = Integer.MAX_VALUE;
+			int j;
+			for(j = i-1; j >= 0; j--){
+				if(days[i] - days[j] >= 7){
+					break;
+				}
+			}
+			c2 = j >= 0 ? dp[j] + costs[1] : costs[1];
+			for(j = i-1; j >= 0; j--){
+				if(days[i] - days[j] >= 30){
+					break;
+				}
+			}
+			c3 = j >= 0 ? dp[j-1] + costs[2] : costs[2];
+
+			dp[i] = Math.min(c1, Math.min(c2, c3));
+		}
+		return dp[days.length-1];
+	}
+
     /**
      * 994 腐烂的橘子
      * 多源广度优先
