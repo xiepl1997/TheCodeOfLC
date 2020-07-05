64b103cb5a11780ae755cfaace71439e8474d3c7
xiepl1997 <xiepl1997@gmail.com>
2020-07-05 19:12:07 +0800

    add 572 判断是否是另一个树的子树

--- src/xpl/LeetCodes.java
+++ src/xpl/LeetCodes.java
@@ -4359,6 +4359,22 @@ public class LeetCodes {
         return false;
     }
 
+	public boolean isSubtree572_2(TreeNode s, TreeNode t) {
+		if (t == null) return true;   // t 为 null 一定都是 true
+		if (s == null) return false;  // 这里 t 一定不为 null, 只要 s 为 null，肯定是 false
+		return isSubtree572_2(s.left, t) || isSubtree572_2(s.right, t) || isSameTree_572(s,t);
+	}
+
+	/**
+	 * 判断两棵树是否相同
+	 */
+	public boolean isSameTree_572(TreeNode s, TreeNode t){
+		if (s == null && t == null) return true;
+		if (s == null || t == null) return false;
+		if (s.val != t.val) return false;
+		return isSameTree_572(s.left, t.left) && isSameTree_572(s.right, t.right);
+	}
+
 
 	/**
 	 * 590 N叉树的后序遍历
